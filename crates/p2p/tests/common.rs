3046b4b68d65b904f7c428623f1d852a47c97173
Kyrylo Baibula <64193423+Velnbur@users.noreply.github.com>
2025-01-16 14:51:54 +0200

    Add async threadpooled wrapper for `sled::Db` (#16)

--- crates/p2p/tests/common.rs
+++ crates/p2p/tests/common.rs
@@ -1,12 +1,13 @@
-use std::time::Duration;
+use std::{sync::Arc, time::Duration};
 
 use libp2p::{identity::secp256k1::Keypair as SecpKeypair, Multiaddr, PeerId};
 use snafu::ResultExt;
 use strata_p2p::swarm::{self, handle::P2PHandle, P2PConfig, P2P};
+use strata_p2p_db::sled::AsyncDB;
 use tokio_util::sync::CancellationToken;
 
 pub struct Operator {
-    pub p2p: P2P<(), sled::Db>,
+    pub p2p: P2P<(), AsyncDB>,
     pub handle: P2PHandle<()>,
 }
 
@@ -34,7 +35,8 @@ impl Operator {
 
         let swarm = swarm::with_inmemory_transport(&config)
             .whatever_context("failed to initialize swarm")?;
-        let (p2p, handle) = P2P::<(), sled::Db>::from_config(config, cancel, db, swarm)
+        let db = AsyncDB::new(Default::default(), Arc::new(db));
+        let (p2p, handle) = P2P::<(), AsyncDB>::from_config(config, cancel, db, swarm)
             .whatever_context("invalid p2p config")?;
 
         Ok(Self { handle, p2p })
