bb3eeecf37f62adef97bd13fcf2a9261511406ca
Jose Storopoli <jose@storopoli.com>
2025-02-13 15:35:36 -0300

    feat(types): StakeChainId

--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -3,9 +3,11 @@
 mod operator;
 mod scope;
 mod session_id;
+mod stake_chain_id;
 mod wots;
 
 pub use operator::OperatorPubKey;
 pub use scope::Scope;
 pub use session_id::SessionId;
+pub use stake_chain_id::StakeChainId;
 pub use wots::Wots256PublicKey;
