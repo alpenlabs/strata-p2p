a51086bc9d13611e639d39bdf89a7b990e4c6b40
ProofOfKeags <keagan.mcclelland@gmail.com>
2025-02-26 05:39:33 -0700

    implement Stream for P2PHandle (#28)
    * implement Stream for P2PHandle
    
    * propagate dropped message errors

--- crates/p2p/src/swarm/handle.rs
+++ crates/p2p/src/swarm/handle.rs
@@ -1,12 +1,24 @@
 //! Entity to control P2P implementation, spawned in another async task,
 //! and listen to its events through channels.
 
+use std::fmt::Display;
+
+use futures::{FutureExt, Stream};
+use thiserror::Error;
 use tokio::sync::{
     broadcast::{self, error::RecvError},
     mpsc,
 };
 
 use crate::{commands::Command, events::Event};
+
+#[derive(Debug, Clone, Error)]
+pub struct ErrDroppedMsgs(u64);
+impl Display for ErrDroppedMsgs {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "P2PHandle dropped {} messages", self.0)
+    }
+}
 
 /// Handle to interact with P2P implementation spawned in another async
 /// task. To create a new one, use [`super::P2P::new_handle`].
@@ -47,5 +59,28 @@ where
     /// Checks if the event's channel is empty or not.
     pub fn events_is_empty(&self) -> bool {
         self.events.is_empty()
+    }
+}
+
+impl<DSP> Stream for P2PHandle<DSP>
+where
+    DSP: prost::Message + Clone,
+{
+    type Item = Result<Event<DSP>, ErrDroppedMsgs>;
+
+    fn poll_next(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        let poll = Box::pin(self.next_event()).poll_unpin(cx);
+        match poll {
+            std::task::Poll::Ready(Ok(v)) => std::task::Poll::Ready(Some(Ok(v))),
+            std::task::Poll::Ready(Err(RecvError::Closed)) => std::task::Poll::Ready(None),
+            std::task::Poll::Ready(Err(RecvError::Lagged(skipped))) => {
+                tracing::warn!("P2P Stream lost {} messages", skipped);
+                std::task::Poll::Ready(Some(Err(ErrDroppedMsgs(skipped))))
+            }
+            std::task::Poll::Pending => std::task::Poll::Pending,
+        }
     }
 }
