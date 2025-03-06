4008a49e67fe9ec1b6576ecad15fd25f7d9de491
Jose Storopoli <jose@storopoli.com>
2025-03-06 09:31:59 -0300

    fix: remove unused StakeData

--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -9,7 +9,6 @@ mod operator;
 mod scope;
 mod session_id;
 mod stake_chain_id;
-mod stake_data;
 mod wots;
 
 pub use deposit_data::{Groth16PublicKeys, WotsPublicKeys};
@@ -17,5 +16,4 @@ pub use operator::OperatorPubKey;
 pub use scope::Scope;
 pub use session_id::SessionId;
 pub use stake_chain_id::StakeChainId;
-pub use stake_data::StakeData;
 pub use wots::{Wots160PublicKey, Wots256PublicKey, WOTS_SINGLE};
