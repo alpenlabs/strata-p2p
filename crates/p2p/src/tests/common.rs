3536945d5c85d65d1e768b536e39ab7e8cfb628b
dovgopoly <idovgopoly@gmail.com>
2025-06-17 01:48:35 +0300

    run single gossipsub test

--- crates/p2p/src/tests/common.rs
+++ crates/p2p/src/tests/common.rs
@@ -17,14 +17,12 @@ use libp2p::{
     identity::{secp256k1::Keypair as SecpKeypair, Keypair},
     Multiaddr, PeerId,
 };
-use musig2::{NonceSeed, PartialSignature, PubNonce, SecNonce};
-use strata_p2p_types::{P2POperatorPubKey, Scope, SessionId, StakeChainId, WotsPublicKeys};
-use strata_p2p_wire::p2p::v1::{GossipsubMsg, UnsignedGossipsubMsg};
+use strata_p2p_types::{P2POperatorPubKey, Scope, SessionId};
 use tokio_util::{sync::CancellationToken, task::TaskTracker};
 use tracing::info;
 
 use crate::{
-    commands::{Command, UnsignedPublishMessage},
+    commands::{Command},
     events::Event,
     swarm::{self, handle::P2PHandle, P2PConfig, P2P},
 };
@@ -122,52 +120,52 @@ impl Setup {
         })
     }
 
-    /// Spawn `n` operators that are connected "all-to-all" with handles to them, task tracker
-    /// to stop control async tasks they are spawned in with an extra signers allowlist.
-    pub(crate) async fn with_extra_signers(
-        number: usize,
-        extra_signers: Vec<P2POperatorPubKey>,
-    ) -> anyhow::Result<Self> {
-        let (keypairs, peer_ids, multiaddresses) =
-            Self::setup_keys_ids_addrs_of_n_operators(number);
-
-        let cancel = CancellationToken::new();
-        let mut operators = Vec::new();
-        let mut signers_allowlist: Vec<P2POperatorPubKey> = keypairs
-            .clone()
-            .into_iter()
-            .map(|kp| kp.public().clone().into())
-            .collect();
-
-        // Add the extra signers to the allowlist
-        signers_allowlist.extend(extra_signers);
-
-        for (idx, (keypair, addr)) in keypairs.iter().zip(&multiaddresses).enumerate() {
-            let mut other_addrs = multiaddresses.clone();
-            other_addrs.remove(idx);
-            let mut other_peerids = peer_ids.clone();
-            other_peerids.remove(idx);
-
-            let operator = Operator::new(
-                keypair.clone(),
-                other_peerids,
-                other_addrs,
-                addr.clone(),
-                cancel.child_token(),
-                signers_allowlist.clone(),
-            )?;
-
-            operators.push(operator);
-        }
-
-        let (operators, tasks) = Self::start_operators(operators).await;
-
-        Ok(Self {
-            cancel,
-            tasks,
-            operators,
-        })
-    }
+    // /// Spawn `n` operators that are connected "all-to-all" with handles to them, task tracker
+    // /// to stop control async tasks they are spawned in with an extra signers allowlist.
+    // pub(crate) async fn with_extra_signers(
+    //     number: usize,
+    //     extra_signers: Vec<P2POperatorPubKey>,
+    // ) -> anyhow::Result<Self> {
+    //     let (keypairs, peer_ids, multiaddresses) =
+    //         Self::setup_keys_ids_addrs_of_n_operators(number);
+    //
+    //     let cancel = CancellationToken::new();
+    //     let mut operators = Vec::new();
+    //     let mut signers_allowlist: Vec<P2POperatorPubKey> = keypairs
+    //         .clone()
+    //         .into_iter()
+    //         .map(|kp| kp.public().clone().into())
+    //         .collect();
+    //
+    //     // Add the extra signers to the allowlist
+    //     signers_allowlist.extend(extra_signers);
+    //
+    //     for (idx, (keypair, addr)) in keypairs.iter().zip(&multiaddresses).enumerate() {
+    //         let mut other_addrs = multiaddresses.clone();
+    //         other_addrs.remove(idx);
+    //         let mut other_peerids = peer_ids.clone();
+    //         other_peerids.remove(idx);
+    //
+    //         let operator = Operator::new(
+    //             keypair.clone(),
+    //             other_peerids,
+    //             other_addrs,
+    //             addr.clone(),
+    //             cancel.child_token(),
+    //             signers_allowlist.clone(),
+    //         )?;
+    //
+    //         operators.push(operator);
+    //     }
+    //
+    //     let (operators, tasks) = Self::start_operators(operators).await;
+    //
+    //     Ok(Self {
+    //         cancel,
+    //         tasks,
+    //         operators,
+    //     })
+    // }
 
     /// Create `n` random keypairs, peer ids from them and sequential in-memory
     /// addresses.
@@ -214,196 +212,196 @@ impl Setup {
         (levers, tasks)
     }
 }
-
-pub(crate) fn mock_stake_chain_info(kp: &SecpKeypair, stake_chain_id: StakeChainId) -> Command {
-    let kind = UnsignedPublishMessage::StakeChainExchange {
-        stake_chain_id,
-        // some random point
-        operator_pk: XOnlyPublicKey::from_slice(&[2u8; 32]).unwrap(),
-        pre_stake_txid: Txid::all_zeros(),
-        pre_stake_vout: 0,
-    };
-    kind.sign_secp256k1(kp).into()
-}
-
-pub(crate) fn mock_deposit_setup(kp: &SecpKeypair, scope: Scope) -> Command {
-    let mock_bytes = [0u8; 1_360 + 362_960];
-    let unsigned = UnsignedPublishMessage::DepositSetup {
-        scope,
-        index: 0,
-        hash: sha256::Hash::const_hash(b"hash me!"),
-        funding_txid: Txid::all_zeros(),
-        funding_vout: 0,
-        operator_pk: XOnlyPublicKey::from_slice(&[2u8; 32]).unwrap(),
-        wots_pks: WotsPublicKeys::from_flattened_bytes(&mock_bytes),
-    };
-    unsigned.sign_secp256k1(kp).into()
-}
-
-pub(crate) fn mock_deposit_nonces(kp: &SecpKeypair, session_id: SessionId) -> Command {
-    let unsigned = UnsignedPublishMessage::Musig2NoncesExchange {
-        session_id,
-        pub_nonces: (0..5).map(|_| generate_pubnonce()).collect(),
-    };
-    unsigned.sign_secp256k1(kp).into()
-}
-
-pub(crate) fn mock_deposit_sigs(kp: &SecpKeypair, session_id: SessionId) -> Command {
-    let unsigned = UnsignedPublishMessage::Musig2SignaturesExchange {
-        session_id,
-        partial_sigs: (0..5).map(|_| generate_partial_signature()).collect(),
-    };
-    unsigned.sign_secp256k1(kp).into()
-}
-
-pub(crate) async fn exchange_stake_chain_info(
-    operators: &mut [OperatorHandle],
-    operators_num: usize,
-    stake_chain_id: StakeChainId,
-) -> anyhow::Result<()> {
-    for operator in operators.iter() {
-        operator
-            .handle
-            .send_command(mock_stake_chain_info(&operator.kp, stake_chain_id))
-            .await;
-    }
-    for operator in operators.iter_mut() {
-        // received stake chain info from other n-1 operators
-        for _ in 0..operators_num - 1 {
-            let event = operator.handle.next_event().await?;
-
-            if !matches!(
-                event,
-                Event::ReceivedMessage(GossipsubMsg {
-                    unsigned: UnsignedGossipsubMsg::StakeChainExchange { .. },
-                    ..
-                })
-            ) {
-                bail!("Got event other than 'stake_chain_info' - {:?}", event);
-            }
-        }
-
-        assert!(operator.handle.events_is_empty());
-    }
-
-    Ok(())
-}
-
-pub(crate) async fn exchange_deposit_setup(
-    operators: &mut [OperatorHandle],
-    operators_num: usize,
-    scope: Scope,
-) -> anyhow::Result<()> {
-    for operator in operators.iter() {
-        operator
-            .handle
-            .send_command(mock_deposit_setup(&operator.kp, scope))
-            .await;
-    }
-    for operator in operators.iter_mut() {
-        for _ in 0..operators_num - 1 {
-            let event = operator.handle.next_event().await.unwrap();
-            if !matches!(
-                event,
-                Event::ReceivedMessage(GossipsubMsg {
-                    unsigned: UnsignedGossipsubMsg::DepositSetup { .. },
-                    ..
-                })
-            ) {
-                bail!("Got event other than 'deposit_setup' - {:?}", event);
-            }
-            info!(to=%operator.peer_id, "Got deposit setup");
-        }
-        assert!(operator.handle.events_is_empty());
-    }
-    Ok(())
-}
-
-pub(crate) async fn exchange_deposit_nonces(
-    operators: &mut [OperatorHandle],
-    operators_num: usize,
-    session_id: SessionId,
-) -> anyhow::Result<()> {
-    for operator in operators.iter() {
-        operator
-            .handle
-            .send_command(mock_deposit_nonces(&operator.kp, session_id))
-            .await;
-    }
-    for operator in operators.iter_mut() {
-        for _ in 0..operators_num - 1 {
-            let event = operator.handle.next_event().await.unwrap();
-            if !matches!(
-                event,
-                Event::ReceivedMessage(GossipsubMsg {
-                    unsigned: UnsignedGossipsubMsg::Musig2NoncesExchange { .. },
-                    ..
-                })
-            ) {
-                bail!("Got event other than 'deposit_nonces' - {:?}", event);
-            }
-            info!(to=%operator.peer_id, "Got deposit setup");
-        }
-        assert!(operator.handle.events_is_empty());
-    }
-    Ok(())
-}
-
-pub(crate) async fn exchange_deposit_sigs(
-    operators: &mut [OperatorHandle],
-    operators_num: usize,
-    session_id: SessionId,
-) -> anyhow::Result<()> {
-    for operator in operators.iter() {
-        operator
-            .handle
-            .send_command(mock_deposit_sigs(&operator.kp, session_id))
-            .await;
-    }
-
-    for operator in operators.iter_mut() {
-        for _ in 0..operators_num - 1 {
-            let event = operator.handle.next_event().await.unwrap();
-            if !matches!(
-                event,
-                Event::ReceivedMessage(GossipsubMsg {
-                    unsigned: UnsignedGossipsubMsg::Musig2SignaturesExchange { .. },
-                    ..
-                })
-            ) {
-                bail!("Got event other than 'deposit_sigs' - {:?}", event);
-            }
-            info!(to=%operator.peer_id, "Got deposit sigs");
-        }
-        assert!(operator.handle.events_is_empty());
-    }
-
-    Ok(())
-}
-
-/// Size of the nonce seed in bytes.
-const NONCE_SEED_SIZE: usize = 32;
-
-/// Generates a mock public nonce.
-pub(crate) fn generate_pubnonce() -> PubNonce {
-    let sec_nonce = generate_secnonce();
-
-    sec_nonce.public_nonce()
-}
-
-/// Generates a mock secret nonce.
-pub(crate) fn generate_secnonce() -> SecNonce {
-    let mut nonce_seed_bytes = [0u8; NONCE_SEED_SIZE];
-    OsRng.fill(&mut nonce_seed_bytes);
-    let nonce_seed = NonceSeed::from(nonce_seed_bytes);
-
-    SecNonce::build(nonce_seed).build()
-}
-
-/// Generates a mock partial signature.
-pub(crate) fn generate_partial_signature() -> PartialSignature {
-    let secret_key = SecretKey::new(&mut OsRng);
-
-    PartialSignature::from_slice(secret_key.as_ref())
-        .expect("should be able to generate arbitrary partial signature")
-}
+//
+// pub(crate) fn mock_stake_chain_info(kp: &SecpKeypair, stake_chain_id: StakeChainId) -> Command {
+//     let kind = UnsignedPublishMessage::StakeChainExchange {
+//         stake_chain_id,
+//         // some random point
+//         operator_pk: XOnlyPublicKey::from_slice(&[2u8; 32]).unwrap(),
+//         pre_stake_txid: Txid::all_zeros(),
+//         pre_stake_vout: 0,
+//     };
+//     kind.sign_secp256k1(kp).into()
+// }
+//
+// pub(crate) fn mock_deposit_setup(kp: &SecpKeypair, scope: Scope) -> Command {
+//     let mock_bytes = [0u8; 1_360 + 362_960];
+//     let unsigned = UnsignedPublishMessage::DepositSetup {
+//         scope,
+//         index: 0,
+//         hash: sha256::Hash::const_hash(b"hash me!"),
+//         funding_txid: Txid::all_zeros(),
+//         funding_vout: 0,
+//         operator_pk: XOnlyPublicKey::from_slice(&[2u8; 32]).unwrap(),
+//         wots_pks: WotsPublicKeys::from_flattened_bytes(&mock_bytes),
+//     };
+//     unsigned.sign_secp256k1(kp).into()
+// }
+//
+// pub(crate) fn mock_deposit_nonces(kp: &SecpKeypair, session_id: SessionId) -> Command {
+//     let unsigned = UnsignedPublishMessage::Musig2NoncesExchange {
+//         session_id,
+//         pub_nonces: (0..5).map(|_| generate_pubnonce()).collect(),
+//     };
+//     unsigned.sign_secp256k1(kp).into()
+// }
+//
+// pub(crate) fn mock_deposit_sigs(kp: &SecpKeypair, session_id: SessionId) -> Command {
+//     let unsigned = UnsignedPublishMessage::Musig2SignaturesExchange {
+//         session_id,
+//         partial_sigs: (0..5).map(|_| generate_partial_signature()).collect(),
+//     };
+//     unsigned.sign_secp256k1(kp).into()
+// }
+//
+// pub(crate) async fn exchange_stake_chain_info(
+//     operators: &mut [OperatorHandle],
+//     operators_num: usize,
+//     stake_chain_id: StakeChainId,
+// ) -> anyhow::Result<()> {
+//     for operator in operators.iter() {
+//         operator
+//             .handle
+//             .send_command(mock_stake_chain_info(&operator.kp, stake_chain_id))
+//             .await;
+//     }
+//     for operator in operators.iter_mut() {
+//         // received stake chain info from other n-1 operators
+//         for _ in 0..operators_num - 1 {
+//             let event = operator.handle.next_event().await?;
+//
+//             if !matches!(
+//                 event,
+//                 Event::ReceivedMessage(GossipsubMsg {
+//                     unsigned: UnsignedGossipsubMsg::StakeChainExchange { .. },
+//                     ..
+//                 })
+//             ) {
+//                 bail!("Got event other than 'stake_chain_info' - {:?}", event);
+//             }
+//         }
+//
+//         assert!(operator.handle.events_is_empty());
+//     }
+//
+//     Ok(())
+// }
+//
+// pub(crate) async fn exchange_deposit_setup(
+//     operators: &mut [OperatorHandle],
+//     operators_num: usize,
+//     scope: Scope,
+// ) -> anyhow::Result<()> {
+//     for operator in operators.iter() {
+//         operator
+//             .handle
+//             .send_command(mock_deposit_setup(&operator.kp, scope))
+//             .await;
+//     }
+//     for operator in operators.iter_mut() {
+//         for _ in 0..operators_num - 1 {
+//             let event = operator.handle.next_event().await.unwrap();
+//             if !matches!(
+//                 event,
+//                 Event::ReceivedMessage(GossipsubMsg {
+//                     unsigned: UnsignedGossipsubMsg::DepositSetup { .. },
+//                     ..
+//                 })
+//             ) {
+//                 bail!("Got event other than 'deposit_setup' - {:?}", event);
+//             }
+//             info!(to=%operator.peer_id, "Got deposit setup");
+//         }
+//         assert!(operator.handle.events_is_empty());
+//     }
+//     Ok(())
+// }
+//
+// pub(crate) async fn exchange_deposit_nonces(
+//     operators: &mut [OperatorHandle],
+//     operators_num: usize,
+//     session_id: SessionId,
+// ) -> anyhow::Result<()> {
+//     for operator in operators.iter() {
+//         operator
+//             .handle
+//             .send_command(mock_deposit_nonces(&operator.kp, session_id))
+//             .await;
+//     }
+//     for operator in operators.iter_mut() {
+//         for _ in 0..operators_num - 1 {
+//             let event = operator.handle.next_event().await.unwrap();
+//             if !matches!(
+//                 event,
+//                 Event::ReceivedMessage(GossipsubMsg {
+//                     unsigned: UnsignedGossipsubMsg::Musig2NoncesExchange { .. },
+//                     ..
+//                 })
+//             ) {
+//                 bail!("Got event other than 'deposit_nonces' - {:?}", event);
+//             }
+//             info!(to=%operator.peer_id, "Got deposit setup");
+//         }
+//         assert!(operator.handle.events_is_empty());
+//     }
+//     Ok(())
+// }
+//
+// pub(crate) async fn exchange_deposit_sigs(
+//     operators: &mut [OperatorHandle],
+//     operators_num: usize,
+//     session_id: SessionId,
+// ) -> anyhow::Result<()> {
+//     for operator in operators.iter() {
+//         operator
+//             .handle
+//             .send_command(mock_deposit_sigs(&operator.kp, session_id))
+//             .await;
+//     }
+//
+//     for operator in operators.iter_mut() {
+//         for _ in 0..operators_num - 1 {
+//             let event = operator.handle.next_event().await.unwrap();
+//             if !matches!(
+//                 event,
+//                 Event::ReceivedMessage(GossipsubMsg {
+//                     unsigned: UnsignedGossipsubMsg::Musig2SignaturesExchange { .. },
+//                     ..
+//                 })
+//             ) {
+//                 bail!("Got event other than 'deposit_sigs' - {:?}", event);
+//             }
+//             info!(to=%operator.peer_id, "Got deposit sigs");
+//         }
+//         assert!(operator.handle.events_is_empty());
+//     }
+//
+//     Ok(())
+// }
+//
+// /// Size of the nonce seed in bytes.
+// const NONCE_SEED_SIZE: usize = 32;
+//
+// /// Generates a mock public nonce.
+// pub(crate) fn generate_pubnonce() -> PubNonce {
+//     let sec_nonce = generate_secnonce();
+//
+//     sec_nonce.public_nonce()
+// }
+//
+// /// Generates a mock secret nonce.
+// pub(crate) fn generate_secnonce() -> SecNonce {
+//     let mut nonce_seed_bytes = [0u8; NONCE_SEED_SIZE];
+//     OsRng.fill(&mut nonce_seed_bytes);
+//     let nonce_seed = NonceSeed::from(nonce_seed_bytes);
+//
+//     SecNonce::build(nonce_seed).build()
+// }
+//
+// /// Generates a mock partial signature.
+// pub(crate) fn generate_partial_signature() -> PartialSignature {
+//     let secret_key = SecretKey::new(&mut OsRng);
+//
+//     PartialSignature::from_slice(secret_key.as_ref())
+//         .expect("should be able to generate arbitrary partial signature")
+// }
