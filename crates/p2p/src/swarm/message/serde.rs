e0479e7a697f859a4a8d0a5d2938efac4beebec9
Arniiiii <lg3dx6fd@gmail.com>
2025-08-28 16:35:06 +0000

    Fixed (de-)serialization : now less copying and (de-)serializing from/to bytes.
    Fix clippy's warning

--- crates/p2p/src/swarm/message/serde.rs
+++ crates/p2p/src/swarm/message/serde.rs
@@ -3,7 +3,7 @@
 use libp2p::identity::PublicKey;
 
 pub(crate) mod pubkey_serializer {
-    use serde::{self, Deserializer, Serializer, de, ser::SerializeSeq};
+    use serde::{self, Deserializer, Serializer, de};
 
     use super::PublicKey;
 
@@ -11,43 +11,34 @@ pub(crate) mod pubkey_serializer {
     where
         S: Serializer,
     {
-        let bytes = data.encode_protobuf();
-        let mut seq = serializer.serialize_seq(Some(bytes.len()))?;
-        for byte in &bytes {
-            seq.serialize_element(byte)?;
-        }
-        seq.end()
+        serializer.serialize_bytes(&data.encode_protobuf())
     }
 
     pub(crate) fn deserialize<'de, D>(deserializer: D) -> Result<PublicKey, D::Error>
     where
         D: Deserializer<'de>,
     {
-        let bytes: Vec<u8> = serde::Deserialize::deserialize(deserializer)?;
-        PublicKey::try_decode_protobuf(&bytes)
+        let bytes: &[u8] = serde::Deserialize::deserialize(deserializer)?;
+        PublicKey::try_decode_protobuf(bytes)
             .map_err(|_| de::Error::custom("Failed to deserialize pubkey"))
     }
 }
 
 pub(crate) mod signature_serializer {
-    use serde::{self, Deserializer, Serializer, de, ser::SerializeSeq};
+    use serde::{self, Deserializer, Serializer, de};
 
     pub(crate) fn serialize<S>(data: &[u8; 64], serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        let mut seq = serializer.serialize_seq(Some(64))?;
-        for byte in data {
-            seq.serialize_element(byte)?;
-        }
-        seq.end()
+        serializer.serialize_bytes(data)
     }
 
     pub(crate) fn deserialize<'de, D>(deserializer: D) -> Result<[u8; 64], D::Error>
     where
         D: Deserializer<'de>,
     {
-        let bytes: Vec<u8> = serde::Deserialize::deserialize(deserializer)?;
+        let bytes: &[u8] = serde::Deserialize::deserialize(deserializer)?;
         bytes
             .try_into()
             .map_err(|_| de::Error::custom("Signature must be exactly 64 bytes"))
